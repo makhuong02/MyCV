11f48d529e918b79c65510b4bba9284e1206aecb
Khuong Luan <harryluan25@gmail.com>
2024-08-10 16:46:44 +0700

    commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Khuong_avt from "../assets/Khuong_avt"
 
 function Intro() {
     return (
         <div id="appearance">
-            <img id="avatar" src="../assets/Khuong_avt.jpg" alt="" />
+            <img id="avatar" src={Khuong_avt} alt="" />
             <h1 className="text-color">Luan Ma Khuong</h1>
             <p id="role">Intern Software Developer</p>
         </div>
