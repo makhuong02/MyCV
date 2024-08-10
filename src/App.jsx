7853087a4eabba1a106842873c8bcc194e63d207
Khuong Luan <harryluan25@gmail.com>
2024-08-10 16:44:04 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import ProfileLayout from "./ProfileLayout";
-import DetailsLayout from "./DetailsLayout";
+import ProfileLayout from "./components/ProfileLayout";
+import DetailsLayout from "./components/DetailsLayout";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import BrowserRouter from 'react-router-dom/BrowserRouter';
 import App from './App.jsx'
 import './style.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>,
 )
